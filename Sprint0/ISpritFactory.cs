e03014cf6fcd6cc0901c94d41e0c4c80b7f6e725
Anthony Cavallero <cavallero.1@buckeyemail.osu.edu>
2018-09-14 17:25:58 -0400

    Added a PlayerSpriteFactory, need a potential factory for Items and Enemies. Need to add new sprite classes for the sprite factories. PlayerSpriteFactory is No-op until then.

---
+++ Sprint0/ISpritFactory.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    public interface ISpriteFactory
+    {
+        public void LoadPlayerTextures(ContentManager contentManager);
+    }
+}
---
+++ Sprint0/PlayerSpriteFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace sprint0
+{
+    public class PlayerSpriteFactory : ISpriteFactory
+    {
+        private Texture2D marioSpriteSheet;
+
+        public void LoadPlayerTextures(ContentManager contentManager)
+        {
+            marioSpriteSheet = contentManager.Load<Texture2D>("marioSMW");
+        }
+
+        public ISprite CreateMarioSprite()
+        {
+            //No -op for now, need to make a sprite class for mario. This is "Big Mario"
+        }
+
+        public ISprite CreateSmallMarioSprite()
+        {
+            //No-op for now, need to make a sprite class for small mario.
+        }
+
+        public ISprite CreateFireMarioSprite()
+        {
+            //No-op for now, need to make a sprite class for fire mario.
+        }
+
+        public ISprite CreateDeadMarioSprite()
+        {
+            //No-op for now, need to make a sprite class for dead mario.
+        }
+    }
+}
