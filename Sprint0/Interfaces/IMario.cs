03d5bb3a328088c8d50946b6312c144bad5aaa90
Anthony Cavallero <909655@gmail.com>
2018-09-24 17:34:54 -0400

    Added prelim for Mario Class, and added my Maintainability review in code reviews

---
+++ Sprint0/Interfaces/IMario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Interfaces
+{
+    interface IMario
+    {
+        string GetMovementState();
+        string GetConditionState();
+        ISprite GetSprite();
+        void SetMovementState(string movement);
+        void SetConditionState(string condition);
+        ISprite SetSprite();
+       
+    }
+}
---
+++ Sprint0/Mario.cs
@@ -0,0 +1,58 @@
+﻿using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    class Mario : IMario
+    {
+        private static Mario instance = new Mario();
+
+        string marioMovement;
+        string marioCondition;
+        ISprite marioSprite;
+
+        public static Mario Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public string GetConditionState()
+        {
+            return marioCondition;
+        }
+
+        public string GetMovementState()
+        {
+            return marioMovement;
+        }
+
+        public ISprite GetSprite()
+        {
+            return marioSprite;
+        }
+
+        public void SetConditionState(string condition)
+        {
+            marioCondition = condition;
+        }
+
+        public void SetMovementState(string movement)
+        {
+            marioMovement = movement;
+        }
+
+        public ISprite SetSprite()
+        {
+                //marioSprite = PlayerSpriteFactory.Instance.GetPlayerSprite(marioMovement,marioCondition);
+                //Work in progress for solution to state explosion
+            throw new NotImplementedException();
+        }
+    }
+}
