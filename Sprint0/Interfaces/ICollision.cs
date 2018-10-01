c951a16c5a81f1a7193a689dd4976eb139dbf1f7
Jacob Starrett <starrett.166@osu.edu>
2018-10-01 15:26:19 -0400

    added skeleton Collision detection interfaces to work on later

---
+++ Sprint0/Interfaces/ICollision.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Interfaces
+{
+    public interface ICollision
+    {
+        /* So the idea here is to have the handler call GetType() and get back like MarioAndEnemy (or maybe KoopaAndKoopa down the line), then it knows
+         * what the first and second entities should be. Thus, it should cast GetFirstEntity() and GetSecondEntity() to what it knows they should be and
+         then have that specific handler do whatever else it does */
+
+        string GetType(); //I feel like this should return one of like several enums: MarioAndEnemy, MarioAndBlock, MarioAndItem (maybe handle during refactor?)
+        Rectangle GetOverlap();
+        string GetFirstEntityRelativePosition(); //same as above
+        Object GetFirstEntity();
+        Object GetSecondEntity();
+
+    }
+}
---
+++ Sprint0/Interfaces/ICollisionDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Interfaces
+{
+    public interface ICollisionDetector
+    {
+        //REMOVEME for this sprint nothing can collide with anything except mario. Returning one ICollision may be weird if two pairs repeatedly collide for 
+        //consecutive frames but not a worry this sprint. Later, might want this to be a queue of ICollisions.
+        ICollision ScanForCollisions(IMario mario, List<IEnemy> entities);
+        ICollision ScanForCollisions(IMario mario, List<IItem> entities);
+        ICollision ScanForCollisions(IMario mario, List<IBlock> entities);
+    }
+}
