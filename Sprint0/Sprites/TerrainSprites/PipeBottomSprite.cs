2f12b03900ad8c97f2ce0661a7848705bc4bf57b
Marie Pendley <mariependley@gmail.com>
2018-10-31 12:21:53 -0400

    made pipe bottom

---
+++ Sprint0/Sprites/TerrainSprites/PipeBottomSprite.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SuperPixelBrosGame
+{
+    public class PipeBodySprite : ISprite
+    {
+
+        private Texture2D spriteSheet;
+        private const int SIZE_SCALAR = 2;
+
+        public PipeBodySprite(Texture2D texture)
+        {
+            spriteSheet = texture;
+        }
+
+        public void Update()
+        {
+            //no-op one frame
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, Color color)
+        {
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle;
+
+            sourceRectangle = new Rectangle(86, 67, 32, 16);
+            destinationRectangle = new Rectangle((int)location.X, (int)location.Y, 32 * SIZE_SCALAR, 32 * SIZE_SCALAR);
+
+            spriteBatch.Draw(spriteSheet, destinationRectangle, sourceRectangle, color);
+        }
+
+        public Rectangle GetHitboxFromSprite(Vector2 location)
+        {
+            return new Rectangle((int)location.X, (int)location.Y, 32 * SIZE_SCALAR, 32 * SIZE_SCALAR);
+        }
+    }
+}
