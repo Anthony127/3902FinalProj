19bf41abad37741ea91467db98038fee10193c32
Marie Pendley <mariependley@gmail.com>
2018-11-07 21:37:13 -0500

    create hud and scorekeeper

---
+++ Sprint0/HUD/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Interfaces;
+using Sprint0.MasterClasses.BaseClasses;
+using SuperPixelBrosGame.Interfaces;
+using SuperPixelBrosGame.Level;
+
+namespace SuperPixelBrosGame.HUD
+{
+    class ScoreKeeper
+    {
+        private int time;
+        private int score;
+        private int coins;
+        private ScoreKeeper instance = new ScoreKeeper();
+        public ScoreKeeper Instance()
+        {
+            return instance;
+        }
+
+        private ScoreKeeper()
+        {
+            time = 0;
+            score = 0;
+            coins = 0;
+        }
+    }
+}
