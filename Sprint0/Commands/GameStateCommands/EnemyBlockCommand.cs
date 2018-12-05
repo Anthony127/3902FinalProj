da1eecc825561e360eebacbeb5aed2fd6510fe0c
Marie Pendley <mariependley@gmail.com>
2018-12-04 21:00:20 -0500

    added enemy block, enemy block command

---
+++ Sprint0/Commands/GameStateCommands/EnemyBlockCommand.cs
@@ -0,0 +1,40 @@
+﻿using SuperPixelBrosGame.Interfaces;
+using SuperPixelBrosGame.Level;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperPixelBrosGame
+{
+    public class EnemyBlockCommand : ICommand
+    {
+        private SuperPixelBrosGame SuperPixelBrosGame;
+        private PasswordInputController controller;
+
+        public EnemyBlockCommand(SuperPixelBrosGame SuperPixelBrosGame, PasswordInputController controller)
+        {
+            this.SuperPixelBrosGame = SuperPixelBrosGame;
+            this.controller = controller;
+        }
+
+        public void Execute()
+        {
+            List<IBlock> removedBlocks = new List<IBlock>();
+            List<IBlock> addedBlocks = new List<IBlock>();
+            Random rnd = new Random();
+            foreach (IBlock block in PlayerLevel.Instance.BlockArray)
+            {
+                if (block is QuestionBlock)
+                {
+                    if (rnd.Next(0, 1) == 1)
+                    {
+                        removedBlocks.Add(block);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Sprint0/MasterClasses/EnemyBlock.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.MasterClasses.BaseClasses;
+using SuperPixelBrosGame.Interfaces;
+using SuperPixelBrosGame.Level;
+using SuperPixelBrosGame.MasterClasses;
+using SuperPixelBrosGame.Sprites;
+using SuperPixelBrosGame.States.Blocks;
+using SuperPixelBrosGame.States.Mario.Condition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperPixelBrosGame
+{
+    class EnemyBlock : Block, IBlock, ICollidable
+    {
+        public void SpawnEnemy()
+        {
+            SoundFactory.Instance.PlaySoundEffect("SOUND_POWERUP_APPEARS");
+                IEnemy enemy = new Goomba
+                {
+                    Location = new Vector2(Location.X, Location.Y - SpriteUtility.Instance.BLOCK_UNIT)
+                };
+                Level.PlayerLevel.Instance.EnemyArray.Add(enemy);
+        }
+
+        public override void Bump()
+        {
+            BlockState.Bump();
+        }
+    }
+}
