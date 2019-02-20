f688fc6260614b90aff5345d733bcb68079ebaa5
JJStarrett <jstarrett@jumpmind.com>
2019-02-20 17:11:37 -0500

    Fixed lives resetting on death

--- Sprint0/SuperPixelBrosGame.cs
+++ Sprint0/SuperPixelBrosGame.cs
@@ -131,7 +131,7 @@ namespace SuperPixelBrosGame
             Mario.Instance.ConditionState = new SmallMarioState(Mario.Instance);
             Mario.Instance.MovementState = new MarioRightIdleState(Mario.Instance);
             Mario.Instance.UnloadStarMario();
-            ScoreKeeper.Instance.ResetToGameStart();
+            ScoreKeeper.Instance.ResetToLevelStart();
 
         }
 
